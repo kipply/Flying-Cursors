351fd10a4e54f5110ec9191e9a662259d0fc7cfe
Carol Chen <carolyfisher@gmail.com>
2016-06-25 12:26:09 -0400

    A liiitttle better

--- scripts.js
+++ scripts.js
@@ -1,39 +1,26 @@
 $(document).ready(function () {
-    (function myLoop(i) {         
-        setTimeout(function () {  
-                generateCursor();
-            animateDiv();//  your code here                
-                if (--i) myLoop(i);
-            }, 3000)
-        })(7);    
-    });
-function generateCursor() {
     
-    $( ".a" ).clone().appendTo( "#container" );
-}
+    function makeNewPosition($container) {
 
-function makeNewPosition($container) {
+        // Get viewport dimensions (remove the dimension of the div)
+        $container = ($container || $(window))
+        var h = $container.height() - 50;
+        var w = $container.width() - 50;
 
-    // Get viewport dimensions (remove the dimension of the div)
-    $container = ($container || $(window))
-    var h = $container.height() - 50;
-    var w = $container.width() - 50;
+        var nh = Math.floor(Math.random() * h);
+        var nw = Math.floor(Math.random() * w);
 
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
+        return [nh, nw];
 
-    return [nh, nw];
+    }
 
-}
-
-function animateDiv( ) {
+    function animateDiv( ) {
 
     $( ".a" ).each(function( i ) {
         var newq = makeNewPosition($(this).parent()),
-            oldq = $(this).offset(),
             speed = Math.random() * 5000;
 
-        $(this).animate({
+        $(this).velocity({
             top: newq[0],
             left: newq[1]
         }, speed, function () {
@@ -41,3 +28,11 @@ function animateDiv( ) {
         });
     });
 }
+    (function myLoop(i) {
+        setTimeout(function () {
+        $( ".a" ).clone().appendTo( "#container" );
+            animateDiv();//  your code here
+                if (--i) myLoop(i);
+            }, 3000);
+        })(8);
+    });
