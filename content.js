3601c2a44683db0afecd70e5521b05d2fa8577e5
Carol Chen <carolyfisher@gmail.com>
2016-06-25 17:52:21 -0400

    Now it's a plugin!

--- content.js
+++ content.js
@@ -1,5 +1,12 @@
+
 $(document).ready(function () {
-    
+    $("body html").css({"height":"100%", "margin":"0"});
+    var url = chrome.extension.getURL('cursor.ani');
+    $( "html" ).append( "<div id='container'><img src ='
+        background-image:url('chrome-extension://__MSG_@@extension_id__/fakecursor.png');' class='cursor'></div> ");
+    $("#container").css({"min-height":"100%", "background-color": "aqua",  "max-height":"100%", "max-width":"100%"});
+    $(".cursor").css({"position":"fixed"});
+
    function makeNewPosition($container) {
         var nh = Math.random() * 98,
             nw = Math.random() * 98;
@@ -26,6 +33,6 @@ $(document).ready(function () {
         $( ".cursor" ).clone().appendTo( "#container" );
             animateDiv();//  your code here
                 if (--i) laLoop(i);
-            }, 3000);
-        })(9);
+            }, 5000);
+        })(6);
     });
