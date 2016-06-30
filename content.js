7f73a4a847f76a59a72d4bcc557fc78a0360d20e
Carol Chen <carolyfisher@gmail.com>
2016-06-30 05:54:08 -0400

    commented code

--- content.js
+++ content.js
@@ -1,23 +1,25 @@
 
 $(document).ready(function () {
-//    $("body html").css({"height":"100%", "margin":"0"});
+    //write divs and styles
     $( "body" ).append( "<div id='container'><img style='height: 3%;'' class='cursor' src='https://raw.githubusercontent.com/carolyfisher/Flying-Cursors/master/fakecursor.png''></div> ");
     $("#container").css({"position": "fixed", "z-index" : "1000"});
     $(".cursor").css({"position":"fixed"});
 
+    //generates random position
    function makeNewPosition($container) {
-        var nh = Math.random() * 98,
-            nw = Math.random() * 98;
-        return [nh, nw];
+        var y = Math.random() * 98,
+            x = Math.random() * 98;
+        return [y, x];
 
     }
 
     function animateDiv( ) {
-
+        //animates cursors
         $( ".cursor" ).each(function( i ) {
+            //pulls position and randomizes a speed
             var newq = makeNewPosition($(this).parent()),
                 speed = Math.random() * 5000;
-
+            //velocityJS animation
             $(this).velocity({
                 top: newq[0] +'%',
                 left: newq[1]+'%'
@@ -29,8 +31,8 @@ $(document).ready(function () {
     (function laLoop(i) {
         setTimeout(function () {
         $( ".cursor" ).clone().appendTo( "#container" );
-            animateDiv();//  your code here
+            animateDiv();
                 if (--i) laLoop(i);
-            }, 5000);
-        })(6);
+            }, 5000); //delay between doubling of cursors
+        })(6); //times cursors are doubled
     });
